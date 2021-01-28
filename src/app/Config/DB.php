84ac40e3943a5f4fa6fd61228da21137258082fc
Saleev Artem <artem.spark@gmail.com>
2021-01-28 17:47:03 +0300

    update composer config, create env variables

--- src/app/Config/DB.php
+++ src/app/Config/DB.php
@@ -7,13 +7,18 @@ use mysqli;
 class DB
 {
     private mysqli $connection;
-    private string $host = 'mysql';
-    private string $user = 'root';
-    private string $pwd = 'qwerty';
-    private string $database = 'test_work';
+    private string $host;
+    private string $user;
+    private string $pwd;
+    private string $database;
 
     public function __construct()
     {
+        $this->database = getenv('DB');
+        $this->host = getenv('DB_HOST');
+        $this->user = getenv('DB_USER');;
+        $this->pwd = getenv('DB_PASS');
+
         $this->connection = new mysqli($this->host, $this->user, $this->pwd, $this->database);
         if (!$this->connection) {
             throw new RuntimeException('Can not connect to db');
